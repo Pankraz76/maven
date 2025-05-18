244cb6f5b46d1185dbf886040c6622c3b3604e3b
Vincent Potucek <vpotucek@me.com>
2025-05-18 11:30:00 +0200

    spotbugs-maven-plugin

--- impl/maven-impl/src/main/java/org/apache/maven/impl/ExcludeFromFailureFile.java
+++ impl/maven-impl/src/main/java/org/apache/maven/impl/ExcludeFromFailureFile.java
@@ -11,9 +11,9 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, either express or implied.  See the License for
- * the specific language governing permissions and limitations
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
  * under the License.
  */
 package org.apache.maven.impl;
@@ -37,8 +37,8 @@ import java.util.regex.Pattern;
 
 public class ExcludeFromFailureFile {
 
-    private static final Pattern PMD_PATTERN = Pattern.compile(
-            "PMD Failure: ([\\w\\.]+):(\\d+) Rule:([\\w]+) Priority:\\d+");
+    private static final Pattern PMD_PATTERN =
+            Pattern.compile("PMD Failure: ([\\w\\.]+):(\\d+) Rule:([\\w]+) Priority:\\d+");
 
     public static void main(String[] args) throws IOException {
         Path logPath = Paths.get("pmd.txt"); // path to pmd.log
