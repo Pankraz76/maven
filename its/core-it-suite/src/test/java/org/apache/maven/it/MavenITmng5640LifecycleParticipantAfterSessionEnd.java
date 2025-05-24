7dcd67f3b08e43c6db46792e184e83ee8bcf3c88
Vincent Potucek <vpotucek@me.com>
2025-05-24 22:25:36 +0200

    chore: resolve false negative suppression

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5640LifecycleParticipantAfterSessionEnd.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5640LifecycleParticipantAfterSessionEnd.java
@@ -22,6 +22,8 @@ import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -92,6 +94,7 @@ class MavenITmng5640LifecycleParticipantAfterSessionEnd extends AbstractMavenInt
         // See https://issues.apache.org/jira/browse/MNG-5641
         // verifier.verifyFilePresent( "target/afterSessionStart.txt" );
         verifier.verifyFilePresent("target/afterSessionEnd.txt");
+        assertThat(exception.getMessage(), equalTo(""));
     }
 
     /**
